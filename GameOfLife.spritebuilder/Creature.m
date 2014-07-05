49b3686016f55c810787969f45ff4fc857b1e128
Dhaval <Dhaval@Dhavals-MacBook-Pro.local>
2014-07-05 15:35:02 +0530

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.m
@@ -0,0 +1,14 @@
+#import "Creature.h"
+@implementation Creature
+- (instancetype)initCreature {
+    // since we made Creature inherit from CCSprite, 'super' below refers to CCSprite
+    self = [super initWithImageNamed:@"GameOfLifeAssets/Assets/bubble.png"];
+    
+    if (self) {
+        self.isAlive = NO;
+    }
+    
+    return self;
+}
+
+@end
